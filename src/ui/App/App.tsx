c4c53c21f3820116a6d1baee5bf27ede769040f0
Natacha Ivannikova <natacha.im@gmail.com>
2021-02-08 14:40:19 +0100

    ➕ - Added @types/react-router-dom dependency and create page home

--- src/ui/App/App.tsx
+++ src/ui/App/App.tsx
@@ -1,13 +1,20 @@
 import React  from 'react';
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
+import Home from '../pages/Home';
+import Header from './components/Header';
 import './App.css';
-
-
 
 const App = () => {
 
   return (
     <div className="App">
-      hola
+     <Router>
+        <Header/>
+        <Switch>
+          <Route exact path="/" component={Home} />
+        </Switch>
+        {/* <Footer /> */}
+      </Router>
     </div>
   );
 }
---
+++ src/ui/pages/Home/Home.tsx
@@ -0,0 +1,26 @@
+import React, { useContext } from 'react';
+// import { ContextCocktails } from '../../components/contexts/ContextCocktails';
+// import FormCocktail from '../../components/Molecules/FormCocktail';
+// import RecipeList from '../../components/Molecules/RecipeList';
+import './Home.css';
+
+const Home: React.FC  = () => {
+  // const { recipes } = useContext(ContextCocktails);
+  return (
+    <>
+      <main>
+        hola home
+        {/* <FormCocktail className="FormCocktail" />
+        {recipes.length > 0 && <RecipeList recipes={recipes} />} */}
+      </main>
+    </>
+  );
+};
+
+export default Home;
+
+
+
+
+
+
