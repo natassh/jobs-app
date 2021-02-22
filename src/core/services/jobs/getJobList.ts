189865e2f7a4e7a051d97cad0ca6e281f214135e
Natacha Ivannikova <natacha.im@gmail.com>
2021-02-22 11:12:46 +0100

    ♻️ - Change defoult image

--- src/core/services/jobs/getJobList.ts
+++ src/core/services/jobs/getJobList.ts
@@ -1,6 +1,6 @@
 // import list from "../list.json"
 const getJobList = (description:string, location:string, fullTime:boolean, pageNumber:number) => {
-  const endPoint = `https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?full_time=${fullTime}?description=${description}&location=${location}&page=${pageNumber}`;
+  const endPoint = `https://cors-anywhere.herokuapp.com/https://jobs.github.com/positions.json?full_time=${fullTime}&description=${description}&location=${location}&page=${pageNumber}`;
   const jobsResponseObject = fetch(endPoint);
   const jobsJsonPromise = jobsResponseObject.then(res =>
     res.json()
