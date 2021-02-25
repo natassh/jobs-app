cc13e3ab0b2c29a20fe1c068e3187a489c541c45
Natacha Ivannikova <natacha.im@gmail.com>
2021-02-25 12:54:00 +0100

    🚀 - Added call to real services

--- src/ui/modules/jobs/contexts/helpers/getJobsService.ts
+++ src/ui/modules/jobs/contexts/helpers/getJobsService.ts
@@ -1,6 +1,6 @@
 import {JobType} from '../JobContext';
-//import {getJobList} from "../../../../../core/services/jobs/getJobList";
-import {getJobListFake as getJobList} from "../../../../../core/services/jobs/getJobListFake"
+import {getJobList} from "../../../../../core/services/jobs/getJobList";
+//import {getJobListFake as getJobList} from "../../../../../core/services/jobs/getJobListFake"
 
 export const getJobsService = async (jobs:JobType[]): Promise<JobType[]> => {
   const jobList: JobType[] = await getJobList();
